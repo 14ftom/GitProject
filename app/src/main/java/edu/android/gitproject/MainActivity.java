225195b4adb52b55f349b0214cc6d3a6b4a0077e
14ftom <14ftom@naver.com>
2018-04-13 12:03:20 +0900

    textview added

--- app/src/main/java/edu/android/gitproject/MainActivity.java
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -2,8 +2,11 @@ package edu.android.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
